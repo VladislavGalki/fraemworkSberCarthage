5d0c940ced062ab69c5a921704381bad8d9d4052
Vladislav Galkin <cascascas303@gmail.com>
2021-06-10 21:41:23 +0300

    SberFramework

---
+++ fraemworkSberCarthage/fraemworkSberCarthage/SomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SomeViewController.swift
+//  fraemworkSberCarthage
+//
+//  Created by Владислав Галкин on 10.06.2021.
+//
+import UIKit
+
+open class GSViewController: UIViewController {
+    public func changeBackground(withColor color: UIColor) {
+        self.view.backgroundColor = color
+    }
+}
---
+++ fraemworkSberCarthage/fraemworkSberCarthage/fraemworkSberCarthage.h
@@ -0,0 +1,18 @@
+//
+//  fraemworkSberCarthage.h
+//  fraemworkSberCarthage
+//
+//  Created by Владислав Галкин on 10.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fraemworkSberCarthage.
+FOUNDATION_EXPORT double fraemworkSberCarthageVersionNumber;
+
+//! Project version string for fraemworkSberCarthage.
+FOUNDATION_EXPORT const unsigned char fraemworkSberCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fraemworkSberCarthage/PublicHeader.h>
+
+
